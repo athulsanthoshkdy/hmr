7ca33f626c79fc172badd8622aa50c288168aef3
Athul Santhosh <athulsanthoshkdy@gmail.com>
2021-01-08 18:27:56 +0530

    Update models.py

--- src/models.py
+++ src/models.py
@@ -16,7 +16,7 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
-import tf_slim as slim
+import tensorflow.contrib.slim as slim
 
 from tensorflow.contrib.layers.python.layers.initializers import variance_scaling_initializer
 
